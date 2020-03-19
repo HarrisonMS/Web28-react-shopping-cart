d75a58ded77134559d517b1c3cdf87fa101539cc
Harrison <hmseaborn@gmail.com>
2020-03-19 11:34:00 -0700

    hotfix removed react as well

--- src/contexts/ProductContext.js
+++ src/contexts/ProductContext.js
@@ -2,6 +2,6 @@
 // • Inside that folder create a new file named ProductContext.js
 // In this file, import the createContext function from the react library and create our ProductContext.
 
-import React, { createContext } from 'react';
+import { createContext } from 'react';
 
 export const ProductContext = createContext();
