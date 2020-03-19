4a86ae35ea127277d5ed49414f0d4d23fb8226da
Harrison <hmseaborn@gmail.com>
2020-03-19 11:56:37 -0700

    create CartContext inside context

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,17 +1,16 @@
 import React, { useContext } from 'react';
 import { ProductContext } from '../contexts/ProductContext'
-
 // Components
 import Product from './Product';
 
 
-
 const Products = () => {
   const { products, addItem } = useContext(ProductContext)
+  console.log(products)
 	return (
 		<div className="products-container">
 			{products.map(product => (
-       
+        
 				<Product
 					key={product.id}
 					product={product}
